508c8fa745dc14f0c4d541dd8ce1d25b1f5d3120
jonesleroy <jonesleroy1@gmail.com>
2025-05-29 22:32:12 -0400

    vault

---
+++ index.js
@@ -0,0 +1,13 @@
+let code1 = 7 + 3;
+console.log(code1);
+let code2 = 160 / 2 - 40;
+console.log(code2);
+let code3 = (10 % 3) + 38;
+console.log(code3);
+let message = "The vault has been secured. The combination is: ";
+console.log(message);
+let codeA = code1 + "-" + code2 + "-" + code3;
+console.log(codeA);
+let codeB = `${code1}-${code2}-${code3}`;
+console.log(codeB);
+console.log(`${message} ${codeA} ${codeB}`);
